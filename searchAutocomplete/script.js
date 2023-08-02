0ec0ebfc05d0cc21c481bd026ea00816e44c898c
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2023-08-02 21:40:54 +0300

    🚀 Added code to implement dynamic search suggestions based on user input.
    🔍 Implemented search functionality for suggestion words related to programming languages and technologies.
    
    📊 Enhanced user experience by displaying recent search suggestions using localStorage.
    
    📝 Set up event listeners for input changes and clicks, triggering suggestions and recent search creation.
    
    📜 Implemented form submission prevention and integrated Google search for non-empty input.
    
    🧩 Created a LocalStorage class to manage and store recent search suggestions for better user experience.

---
+++ searchAutocomplete/script.js
@@ -0,0 +1,71 @@
+let suggestionWords = [
+    "html",
+    "css",
+    "javascript",
+    "jquery",
+    "ajax",
+    "react",
+    "angular",
+    "node js",
+    "express js",
+    "redux",
+    "chart js",
+    "bootstrap",
+    "php",
+    "yii",
+    "laravel",
+    "codigniter",
+    "mysql",
+    "mongo db",
+    "asp .net",
+    "java",
+    "python",
+    "django",
+    "ruby",
+    "c++",
+    "webpack",
+    "hammer js",
+    "http",
+    "server",
+    "programming",
+    "artificial inteligence",
+    "development",
+    "website",
+    "app",
+    "frontend",
+    "backend",
+    "cross platform",
+    "xml",
+    "api",
+    "algorithm",
+    "ssl",
+    "enrypt",
+    "decrypt",
+    "code",
+];
+
+const suggestionListEl = document.getElementById("suggestionList");
+const inputTextEl = document.getElementById("inputText");
+const formEl = document.getElementById("form");
+const sectionEl = document.getElementById("section");
+
+let currentPosition = -1;
+
+inputTextEl.addEventListener("input", searchSuggestions);
+inputTextEl.addEventListener("click", createRecentSearchSuggestions);
+
+formEl.addEventListener("submit", (e) => {
+ e.preventDefault();
+ if(inputTextEl.value.trim()) searchGoogle(inputTextEl.value);
+});
+
+// Suggestions are stored in localStorage for better Experience for user
+
+class LocalStorage {
+    constructor(LOCALSTORAGE_KEY) {
+        this.local = localStorage.getItem(LOCALSTORAGE_KEY)
+            ? JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY))
+            : [];
+        this.localKey = LOCALSTORAGE_KEY;
+    }
+}
